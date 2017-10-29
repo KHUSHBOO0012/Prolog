4196dad727d63baacdfd522e4ba9ba9fba41051e
Pro-Panda <f2016015@pilani.bits-pilani.ac.in>
2017-10-29 11:21:09 +0530

    Fix for prompt after displaying true in natCheck

--- natCheck.pl
+++ natCheck.pl
@@ -2,5 +2,5 @@
 % To check whether the number given is a natural number or not.
 
 nat(X):- Y is X-1, X>0, nat(Y).
-nat(X):- X = 1.
-% Waits for a prompt after displaying true if the number is a natural number.
+nat(1).
+% Doesn't wait for the prompt after displaying true as there is no backtracking possible.
